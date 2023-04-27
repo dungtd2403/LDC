0f5ed4b2a072e68220949168f967c4d396a1ae69
dungtd2403 <dung.td2403@gmail.com>
2023-04-27 09:05:38 +0700

    UPLOAD: calculate mean

---
+++ mean_cal.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+
+import glob
+from glob import glob
+
+img = cv2.imread("/home/dung/Project/LDC/data/1.JPG")
+a = np.average(img, axis=(0,1))
+
+def glob_img_mean(images_dir):
+    mean_list =[]
+    # count = 0
+    # mean = [0,0,0]
+    images = sorted(glob(f"{images_dir}/*.JPG"))
+    for img in images:
+        img = cv2.imread("/home/dung/Project/LDC/data/1.JPG")
+        # print(img.shape)
+        mean = np.average(img, axis=(0,1))
+        # mean += mean
+        mean_list.append(mean)
+        # count += 1
+        mean_all = sum(mean_list) / len(mean_list)
+    return mean_all
+if __name__ == "__main__":
+    img_dir = '/home/dung/Project/LDC/data'
+    mean = glob_img_mean(img_dir)
+    print(mean)
